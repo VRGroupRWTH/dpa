6b5848a3335159608441b48a975ef65dcbcff9c0
acdemiralp <demiralpali@gmail.com>
2019-12-09 12:36:23 +0100

    Updated claix module.

--- utility/shell/load_claix_modules.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-module swap intelmpi openmpi
-module load gcc/9 cmake/3.13.2
---
+++ utility/shell/setup_claix.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Enable modules.
+module swap intelmpi openmpi
+module load gcc/9 cmake/3.13.2
+
+# Install git.
+cd ~
+mkdir packages
+cd packages
+
+git clone --branch v2.24.0 https://github.com/git/git.git
+cd git
+make configure
+./configure --prefix=/home/ad784563/packages
+make
+make install
+export PATH=~/packages/bin:$PATH
