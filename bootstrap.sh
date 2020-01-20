2e6920b5927ada447f8810ed7c7e419cdfb3c05e
acdemiralp <demiralpali@gmail.com>
2020-01-20 04:33:09 +0100

    Adjustments.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,13 +6,9 @@ if [ ! -d "vcpkg" ]; then git clone https://github.com/Microsoft/vcpkg.git; fi
 cd vcpkg
 if [ ! -f "vcpkg" ]; then ./bootstrap-vcpkg.sh; fi
 
-if [ ! -d "custom-triplets" ]; then mkdir custom-triplets; fi
-cp ./triplets/x64-linux.cmake ./custom-triplets/x64-linux-static.cmake
-cp ../../utility/vcpkg/x64-linux-dynamic.cmake ./custom-triplets/x64-linux.cmake
-
 VCPKG_DEFAULT_TRIPLET=x64-linux
 vcpkg install boost-mpi boost-odeint boost-ublas catch2 Eigen3 hdf5[parallel] intel-mkl mpi nlohmann-json
-vcpkg install tbb --overlay-triples=custom-triplets
+vcpkg install tbb:x64-linux-dynamic --overlay-triplets=../../utility/vcpkg/
 cd ..
 
 cmake -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake ..
