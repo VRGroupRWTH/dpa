00066ef2e9c0f301ba024a20b254c84391565de9
acdemiralp <demiralpali@gmail.com>
2020-01-21 22:00:31 +0100

    Added uniformly distributed random seed generation.

--- include/dpa/stages/uniform_seed_generator.hpp
+++ include/dpa/stages/uniform_seed_generator.hpp
@@ -1,6 +1,7 @@
 #ifndef DPA_STAGES_UNIFORM_SEED_GENERATOR_HPP
 #define DPA_STAGES_UNIFORM_SEED_GENERATOR_HPP
 
+#include <array>
 #include <optional>
 #include <vector>
 
@@ -12,13 +13,13 @@ namespace dpa
 class uniform_seed_generator
 {
 public:
-  static std::vector<particle<vector3, integer>> generate(vector3 offset, vector3 size, vector3 stride, integer iterations, integer process_index, std::optional<aabb3> aabb = std::nullopt);
+  static std::vector<particle<vector3, integer>> generate       (vector3 offset, vector3 size, vector3                    stride, integer iterations, integer process_index, std::optional<aabb3> aabb = std::nullopt);
+  static std::vector<particle<vector3, integer>> generate_random(vector3 offset, vector3 size, std::size_t                count , integer iterations, integer process_index, std::optional<aabb3> aabb = std::nullopt);
+  static std::vector<particle<vector3, integer>> generate_random(vector3 offset, vector3 size, std::array<std::size_t, 2> range , integer iterations, integer process_index, std::optional<aabb3> aabb = std::nullopt);
 
   // TODO: Seeds from radius, generated within the sphere enclosed by it.
   // TODO: Seeds from vector of particles     (read in parallel, then distributed all to all, also from file of 1D vector3 array).
   // TODO: Seeds from scalar mask of booleans (read in parallel, one particle per true voxel, also from file of 3D boolean array).
-  // TODO: Seeds from number of particles                    , generated randomly according to a distribution (e.g. uniform, normal).
-  // TODO: Seeds from minimum and maximum number of particles, generated randomly according to a distribution (e.g. uniform, normal).
 };
 }
 
--- source/stages/uniform_seed_generator.cpp
+++ source/stages/uniform_seed_generator.cpp
@@ -1,15 +1,18 @@
 #include <dpa/stages/uniform_seed_generator.hpp>
 
+#include <random>
+
 #include <tbb/tbb.h>
 
 #include <dpa/math/indexing.hpp>
+#include <dpa/math/distributions/multivariate_uniform_distribution.hpp>
 
 #undef min
 #undef max
 
 namespace dpa
 {
-std::vector<particle<vector3, integer>> uniform_seed_generator::generate(vector3 offset, vector3 size, vector3 stride, integer iterations, integer process_index, std::optional<aabb3> aabb)
+std::vector<particle<vector3, integer>> uniform_seed_generator::generate       (vector3 offset, vector3 size, vector3                    stride, integer iterations, integer process_index, std::optional<aabb3> aabb)
 {
   if (aabb)
   {
@@ -36,5 +39,46 @@ std::vector<particle<vector3, integer>> uniform_seed_generator::generate(vector3
 #endif
   });
   return particles;
+}
+std::vector<particle<vector3, integer>> uniform_seed_generator::generate_random(vector3 offset, vector3 size, std::size_t                count , integer iterations, integer process_index, std::optional<aabb3> aabb) 
+{
+  if (aabb)
+  {
+    auto intersection = aabb->intersection(aabb3(offset, offset + size));
+    if (!intersection.isEmpty())
+    {
+      offset = intersection.min  ();
+      size   = intersection.sizes();
+    }
+  }
+
+  auto distribution_range = std::initializer_list
+  {
+    std::initializer_list {offset[0], offset[0] + size[0]},
+    std::initializer_list {offset[1], offset[1] + size[1]},
+    std::initializer_list {offset[2], offset[2] + size[2]}
+  };
+
+  std::vector<particle<vector3, integer>> particles(count);
+  tbb::parallel_for(std::size_t(0), particles.size(), std::size_t(1), [&] (const std::size_t index)
+  {
+    static thread_local std::random_device     random_device   ;
+    static thread_local std::mt19937           mersenne_twister;
+    multivariate_uniform_distribution<vector3> distribution(distribution_range);
+
+#ifdef DPA_FTLE_SUPPORT
+    particles[index] = {distribution(mersenne_twister), iterations, relative_direction::center, process_index};
+#else
+    particles[index] = {distribution(mersenne_twister), iterations, relative_direction::center};
+#endif
+  });
+  return particles;
+}
+std::vector<particle<vector3, integer>> uniform_seed_generator::generate_random(vector3 offset, vector3 size, std::array<std::size_t, 2> range , integer iterations, integer process_index, std::optional<aabb3> aabb)
+{
+  std::random_device                         random_device;
+  std::mt19937                               mersenne_twister(random_device());
+  std::uniform_int_distribution<std::size_t> distribution(range[0], range[1]);
+  return generate_random(offset, size, distribution(mersenne_twister), iterations, process_index, aabb);
 }
 }
