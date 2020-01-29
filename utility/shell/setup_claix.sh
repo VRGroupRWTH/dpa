0c1985a4db6559075462922ff60b47d95567c256
acdemiralp <demiralpali@gmail.com>
2020-01-29 11:06:14 +0100

    Updated shell script to use the project folder.

--- utility/shell/setup_claix.sh
+++ utility/shell/setup_claix.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 INITIAL_DIRECTORY=$PWD
+ROOT_DIRECTORY=/hpcwork/rwth0432
 
 # Enable modules.
 module swap intelmpi openmpi/4.0.2
@@ -8,15 +9,26 @@ module load gcc/9 cmake/3.13.2
 module load python/3.6.0
 
 # Install git.
-cd ~
-if [ ! -d "packages" ]; then mkdir packages; fi
+cd $ROOT_DIRECTORY
+
+if [ ! -d "packages" ]; then 
+  mkdir packages; 
+fi
 cd packages
-if [ ! -d "git" ]; then git clone --branch v2.24.0 https://github.com/git/git.git; fi
-cd git
-make configure
-./configure --prefix=/home/ad784563/packages
-make
-make install
-export PATH=~/packages/bin:$PATH
+
+if [ ! -d "git" ]; then 
+  mkdir git
+  cd git
+
+  git clone --branch v2.24.0 https://github.com/git/git.git; 
+  cd git
+  make configure
+  ./configure --prefix=$ROOT_DIRECTORY/packages/git
+  
+  make
+  make install
+fi
+
+export PATH=$ROOT_DIRECTORY/packages/git/bin:$PATH
 
 cd $INITIAL_DIRECTORY
