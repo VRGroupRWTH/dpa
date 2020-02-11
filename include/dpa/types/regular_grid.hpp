f9d417a25e8dc8a4124d4ab19fe0282f8a0cb634
acdemiralp <demiralpali@gmail.com>
2020-02-11 20:26:54 +0100

    Implemented apply_window, and structure tensor on top of it. Added functions for Hessian, Laplacian.

--- include/dpa/types/regular_grid.hpp
+++ include/dpa/types/regular_grid.hpp
@@ -14,18 +14,26 @@
 
 namespace dpa
 {
-// Hessian and Laplacian are available through the gradient operation.
 template <typename _element_type, std::size_t _dimensions>
 struct regular_grid
 {
-  static constexpr std::size_t dimensions = _dimensions;
+  static constexpr auto dimensions = _dimensions;
 
-  using element_type = _element_type;
-  using domain_type  = typename vector_traits<scalar, dimensions>::type;
-  using index_type   = std::array<std::size_t, dimensions>;
+  using element_type          = _element_type;
+
+  using domain_type           = typename vector_traits<scalar  , dimensions>::type;
+  using index_type            = std::array        <std::size_t , dimensions>;
+  using container_type        = boost::multi_array<element_type, dimensions>;
+  using array_view_type       = typename container_type::template array_view<dimensions>::type;
+
+  using gradient_type         = regular_grid<typename gradient_traits <element_type, dimensions>::type, dimensions>;
+  using potential_type        = regular_grid<typename potential_traits<element_type, dimensions>::type, dimensions>;
+  using structure_tensor_type = regular_grid<typename gradient_traits <typename gradient_traits<element_type, dimensions>::type, dimensions>::type, dimensions>;
+  using hessian_type          = regular_grid<typename gradient_traits <typename gradient_traits<element_type, dimensions>::type, dimensions>::type, dimensions>;
+  using laplacian_type        = regular_grid<domain_type, dimensions>;
 
   // Ducks [] on the domain_type.
-  index_type    cell_index (const domain_type& position) const
+  index_type            cell_index      (const domain_type& position) const
   {
     index_type index;
     for (std::size_t i = 0; i < dimensions; ++i)
@@ -33,12 +41,12 @@ struct regular_grid
     return index;
   }
   // Ducks [] on the domain_type.
-  element_type& cell       (const domain_type& position)
+  element_type&         cell            (const domain_type& position)
   {
     return data(cell_index(position));
   }
   // Ducks [] on the domain_type.
-  bool          contains   (const domain_type& position) const
+  bool                  contains        (const domain_type& position) const
   {
     for (std::size_t i = 0; i < dimensions; ++i)
     {
@@ -49,7 +57,7 @@ struct regular_grid
     return true;
   }
   // Ducks [] on the domain_type.
-  element_type  interpolate(const domain_type& position) const
+  element_type          interpolate     (const domain_type& position) const
   {
     domain_type weights    ;
     index_type  start_index;
@@ -74,7 +82,7 @@ struct regular_grid
     return intermediates[0];
   }
 
-  void          apply      (std::function<void(const index_type&, element_type&)> function)
+  void                  apply           (std::function<void(const index_type&, element_type&)> function)
   {
     index_type start_index; start_index.fill(0);
     index_type end_index  ;
@@ -83,15 +91,35 @@ struct regular_grid
       end_index[i] = data.shape()[i];
     parallel_permute_for<index_type>([&] (const index_type& index) { function(index, data(index)); }, start_index, end_index, increment);
   }
-
-  regular_grid<typename gradient_traits <element_type, dimensions>::type, dimensions> gradient ()
+  void                  apply_window    (std::function<void(const index_type&, element_type&, array_view_type&)> function, const index_type& window_size)
   {
-    using gradient_type = regular_grid<typename gradient_traits<element_type, dimensions>::type, dimensions>;
+    apply([&] (const index_type& index, element_type& element)
+    {
+      boost::detail::multi_array::index_gen<dimensions, dimensions> indices;
+      for (auto dimension = 0; dimension < dimensions; ++dimension)
+      {
+        auto& range = indices.ranges_[dimension];
 
+        if (index[dimension] > std::floor(window_size[dimension] / scalar(2)))
+          range.start (index[dimension] - std::floor(window_size[dimension] / scalar(2)));
+        else
+          range.start (0);
+        
+        if (index[dimension] + std::ceil (window_size[dimension] / scalar(2)) < data.shape()[dimension])
+          range.finish(index[dimension] + std::ceil (window_size[dimension] / scalar(2)));
+        else
+          range.finish(data.shape()[dimension]);
+      }
+      function(index, element, data[indices]);
+    });
+  }
+
+  gradient_type         gradient        ()
+  {
     auto& shape       = reinterpret_cast<index_type const&>(*data.shape());
     auto  two_spacing = domain_type(2 * spacing);
 
-    gradient_type gradient {boost::multi_array<typename gradient_type::element_type, dimensions>(shape), offset, size, spacing};
+    gradient_type gradient {gradient_type::container_type(shape), offset, size, spacing};
     gradient.apply([&] (const index_type& index, typename gradient_type::element_type& element)
     {
       for (std::size_t dimension = 0; dimension < dimensions; ++dimension)
@@ -106,10 +134,8 @@ struct regular_grid
     });
     return gradient;
   }
-  regular_grid<typename potential_traits<element_type, dimensions>::type, dimensions> potential() const
+  potential_type        potential       () const
   {
-    using potential_type = regular_grid<typename potential_traits<element_type, dimensions>::type, dimensions>;
-
     auto& shape       = reinterpret_cast<index_type const&>(*data.shape());
     auto  two_spacing = domain_type(2 * spacing);
     
@@ -117,7 +143,7 @@ struct regular_grid
     index_type end_index  ; end_index  .fill(1);
     index_type increment  ; increment  .fill(1);
 
-    potential_type potential {boost::multi_array<typename potential_type::element_type, dimensions>(shape), offset, size, spacing};
+    potential_type potential {potential_type::container_type(shape), offset, size, spacing};
     for (std::size_t dimension = 0; dimension < dimensions; ++dimension)
     {
       for (std::size_t serial_index = 1; serial_index < shape[dimension]; ++serial_index)
@@ -143,12 +169,57 @@ struct regular_grid
     return potential;
   }
 
-  // TODO: Orient Eigenvectors, compute structure tensor.
+  // Scalar-only operator.
+  hessian_type          hessian         ()
+  {
+    return gradient().gradient(); // In generalized formulation of the gradient, transpose is omitted.
+  }
+  // Scalar-only operator.
+  laplacian_type        laplacian       ()
+  {
+    laplacian_type laplacian {laplacian_type::container_type(reinterpret_cast<index_type const&>(*data.shape())), offset, size, spacing};
 
-  boost::multi_array<element_type, dimensions> data    {};
-  domain_type                                  offset  {};
-  domain_type                                  size    {};
-  domain_type                                  spacing {};
+    auto hessian_grid = hessian();
+    hessian_grid.apply([ ] (const index_type& index, typename hessian_type::element_type& element)
+    {
+      laplacian[index] = element.trace();
+    });
+
+    return laplacian;
+  }
+  // Scalar-only operator.
+  structure_tensor_type structure_tensor(const index_type& window_size)
+  {
+    auto gradient_grid = gradient();
+
+    structure_tensor_type outer_product_grid {structure_tensor_type::container_type(reinterpret_cast<index_type const&>(*data.shape())), offset, size, spacing};
+    gradient_grid     .apply       ([&] (const index_type& index, typename gradient_type::element_type& element)
+    {
+      outer_product_grid.data(index) = element.transpose().eval() * element;
+    });
+
+    structure_tensor_type structure_tensor   {structure_tensor_type::container_type(reinterpret_cast<index_type const&>(*data.shape())), offset, size, spacing};
+    outer_product_grid.apply_window([&] (const index_type& index, typename structure_tensor_type::element_type& element, typename structure_tensor_type::array_view_type& elements)
+    {
+      structure_tensor.data(index).setZero();
+
+      index_type start_index; start_index.fill(0);
+      index_type end_index  = window_size;
+      index_type increment  ; increment  .fill(1);
+      permute_for<index_type>([&] (const index_type& relative_index)
+      {
+        structure_tensor.data(index).array() += (elements(relative_index).array() / elements.num_elements()); // TODO: Adjustable weights instead of 1/elements?
+      }, start_index, end_index, increment);  
+    }, window_size);
+
+    return structure_tensor;
+  }
+  // TODO: Orient Eigenvectors (vector/tensor-only operator).
+
+  container_type data    {};
+  domain_type    offset  {};
+  domain_type    size    {};
+  domain_type    spacing {};
 };
 }
 
