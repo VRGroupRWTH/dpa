8328895f48a56c6c5aba617add588be2dea00d92
acdemiralp <demiralpali@gmail.com>
2020-02-12 17:09:02 +0100

    Critical point extractor complete.

--- include/dpa/stages/critical_point_classifier.hpp
+++ include/dpa/stages/critical_point_classifier.hpp
@@ -1,6 +1,8 @@
 #ifndef DPA_STAGES_CRITICAL_POINT_CLASSIFIER_HPP
 #define DPA_STAGES_CRITICAL_POINT_CLASSIFIER_HPP
 
+#include <limits>
+
 #include <dpa/types/point_cloud.hpp>
 #include <dpa/types/regular_fields.hpp>
 
@@ -9,7 +11,7 @@ namespace dpa
 class critical_point_classifier
 {
 public:
-  static point_cloud classify(const regular_vector_field_3d& original_vector_field);
+  static point_cloud classify(const regular_vector_field_3d& vector_field, svector3 resolution, scalar epsilon = std::numeric_limits<scalar>::epsilon());
 };
 }
 
