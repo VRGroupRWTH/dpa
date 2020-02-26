ffe3dae8d7bebc881f1ac596d48eb33414d9e83a
acdemiralp <demiralpali@gmail.com>
2020-02-26 07:20:35 +0100

    Revert "Adjustments."
    This reverts commit cb70c46a717c9f42dab6acfcc5aeac1587b735e4.

--- source/stages/index_generator.cpp
+++ source/stages/index_generator.cpp
@@ -11,48 +11,28 @@ void index_generator::generate(integral_curves& integral_curves, const bool use_
 {
   for (auto& curve : integral_curves)
   {
-    if (use_64_bit)
+    use_64_bit
+      ? curve.indices = std::vector<std::uint64_t>(2 * curve.vertices.size(), std::numeric_limits<std::uint64_t>::max())
+      : curve.indices = std::vector<std::uint32_t>(2 * curve.vertices.size(), std::numeric_limits<std::uint32_t>::max());
+    
+    std::visit([&] (auto& cast_indices) 
     {
-      curve.indices     = std::vector<std::uint64_t>(2 * curve.vertices.size(), std::numeric_limits<std::uint64_t>::max());
-      auto cast_indices = std::get<std::vector<std::uint64_t>>(curve.indices);
-
       tbb::parallel_for(std::size_t(0), curve.vertices.size() - 1, std::size_t(1), [&] (const std::size_t vertex_index)
       {
         if (curve.vertices[vertex_index    ] != terminal_value<vector3>() && 
             curve.vertices[vertex_index + 1] != terminal_value<vector3>())
         {
-          cast_indices.at(2 * vertex_index    ) = vertex_index;
-          cast_indices.at(2 * vertex_index + 1) = vertex_index + 1;
+          cast_indices[2 * vertex_index    ] = vertex_index;
+          cast_indices[2 * vertex_index + 1] = vertex_index + 1;
         }
       });
 
       cast_indices.erase(std::remove(
         cast_indices.begin(), 
         cast_indices.end  (), 
-        std::numeric_limits<std::uint64_t>::max()), 
+        use_64_bit ? std::numeric_limits<std::uint64_t>::max() : std::numeric_limits<std::uint32_t>::max()), 
         cast_indices.end  ());
-    }
-    else
-    {
-      curve.indices     = std::vector<std::uint32_t>(2 * curve.vertices.size(), std::numeric_limits<std::uint32_t>::max());
-      auto cast_indices = std::get<std::vector<std::uint32_t>>(curve.indices);
-
-      tbb::parallel_for(std::size_t(0), curve.vertices.size() - 1, std::size_t(1), [&] (const std::size_t vertex_index)
-      {
-        if (curve.vertices[vertex_index    ] != terminal_value<vector3>() && 
-            curve.vertices[vertex_index + 1] != terminal_value<vector3>())
-        {
-          cast_indices.at(2 * vertex_index    ) = vertex_index;
-          cast_indices.at(2 * vertex_index + 1) = vertex_index + 1;
-        }
-      });
-
-      cast_indices.erase(std::remove(
-        cast_indices.begin(), 
-        cast_indices.end  (), 
-        std::numeric_limits<std::uint32_t>::max()), 
-        cast_indices.end  ());
-    }
+    }, curve.indices);
   }
 }
 }
