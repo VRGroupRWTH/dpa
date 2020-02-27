9ee389bf3bf308d39f63b23edaef884f174c608d
acdemiralp <demiralpali@gmail.com>
2020-02-27 01:51:51 +0100

    Scribbling.

--- utility/python/plot_generator.py
+++ utility/python/plot_generator.py
@@ -1,78 +1,92 @@
 import csv
+import pprint
 
-# Format:
-# [
-#   {rank, name          , iteration_0},
-#   {0   , "total_time"  , 789.0      },
-#   {0   , "round.0.load", 420        },
-#   {0   , "round.0.time", 123.4      },
-#   {0   , "round.1.load", 210        },
-#   {0   , "round.1.time", 456.4      },
-#   ...
-#   {1   , "total_time"  , 789.0      },
-#   {1   , "round.0.load", 420        },
-#   {1   , "round.0.time", 123.4      },
-#   {1   , "round.1.load", 210        },
-#   {1   , "round.1.time", 456.4      },
-#   ...
-# ]
+class auto_resize_list(list):
+  def __setitem__(self, index, value):
+    if index >= len(self):
+      self.extend([None] * (index + 1 - len(self)))
+    list.__setitem__(self, index, value)
+
+"""Format:
+[ # Array ordered by ranks.
+  {
+    "total_time": ...
+    "rounds"    :
+    [ # Array ordered by rounds.
+      {load: ..., time: ..., load_balancing_time: ..., advection_time: ..., communication_time: ...},
+      {load: ..., time: ..., load_balancing_time: ..., advection_time: ..., communication_time: ...},
+      {load: ..., time: ..., load_balancing_time: ..., advection_time: ..., communication_time: ...},
+      ...
+    ]
+  },
+  ...
+]"""
 def parse_csv(filepath):
-  rows = []
+  ranks = auto_resize_list()
   with open(filepath, mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader:
-      rows.append(row)
-    return rows
+      rank  = int  (row["rank"])
+      name  =       row["name"]
+      value = float(row["iteration 0"])
+      
+      if rank >= len(ranks): 
+        ranks[rank] = {"rounds": auto_resize_list()}
 
-# Call on per algorithm per dataset in strong and weak scaling.
-# Call on per algorithm per varying in parameter space.
-# Format: Nodes to maximum total advection time. Example: 
-# {
-#   "16" : 789.0,
-#   "32" : 345.6, 
-#   "64" : 123.4,
-#   "128": 45.6
-# }
-def parse_scaling_benchmark(filepaths):
-  rows = parse_csv(filepath)
-  # TODO
+      if name == "total_time":
+        ranks[rank]["total_time"] = value
+      else:
+        split_name  = name.split(".")
+        round_index = int(split_name[1])
+        value_type  =     split_name[2]
+        if round_index >= len(ranks[rank]["rounds"]):
+          ranks[rank]["rounds"][round_index] = {}
+        ranks[rank]["rounds"][round_index][value_type] = value
+  return ranks
 
-# Call on per algorithm per dataset in load balancing.
-# Format: Rounds to rank to load and time. Example:
-# {
-#   "round_0": 
-#   {
-#     "rank_0":
-#     {
-#       "load": 420,
-#       "time": 123.4
-#     },
-#     ...
-#   },
-#   ...
-# }
-def parse_load_balancing_benchmark(filepath):
-  rows = parse_csv(filepath)
-  # TODO
+"""Format:
+{
+  "nodes"   : [16   , 32   , 64   , 128  , ...]
+  "times"   : [999.9, 666.6, 333.3, 111.1, ...]
+  "speedups": [1    , 1.5  , 3    , 9    , ...]
+}
+"""
+#def parse_scaling_benchmarks(benchmarks):
+  # TODO Parse. Start with the benchmark names to get node counts.
 
-# Computed as t_0 / t_i.
-def compute_speedup(total_benchmark):
-  # TODO
-
-# Computed per round as load_max / load_avg.
-def compute_load_imbalance(load_balancing_benchmark):
-  # TODO
-
-# Call on per dataset in strong and weak scaling.
-# Call on per varying in parameter space.
-# Line plots of ranks against total times.
-def plot_scaling_benchmark(algorithm_total_benchmarks):
-  # TODO
-
-# Call on per algorithm per dataset in load balancing.
-# Gantt chart of ranks against round times.
-def plot_load_balancing_benchmark(load_balancing_benchmark):
-  # TODO
+"""Format:
+[ # Array ordered by rounds.
+  {
+    "boundary"      : ...,
+    "load_imbalance": ...,
+    "times"         :
+    [ # Array ordered by ranks.
+      {load_balancing_time: ..., advection_time: ..., communication_time: ...},
+      {load_balancing_time: ..., advection_time: ..., communication_time: ...},
+      {load_balancing_time: ..., advection_time: ..., communication_time: ...}
+    ]
+  },
+  ...
+]
+"""
+#def parse_load_balancing_benchmark(benchmark):
+  # TODO: Parse. Compute load imbalance.
 
 if __name__ == "__main__":
-  # TODO
+  ranks = parse_csv("C:/Users/demir/Desktop/astro_1024_n_16_l_gllma_d_1.0_s_4,4,4.h5.benchmark.csv")
+  pprint.pprint(ranks)
+  #parse_scaling_benchmarks([
+  #  "C:/Users/demir/Desktop/astro_1024_n_16_l_gllma_d_1.0_s_4,4,4.h5.benchmark.csv",
+  #  "C:/Users/demir/Desktop/astro_1024_n_32_l_gllma_d_1.0_s_4,4,4.h5.benchmark.csv",
+  #  "C:/Users/demir/Desktop/astro_1024_n_64_l_gllma_d_1.0_s_4,4,4.h5.benchmark.csv",
+  #  "C:/Users/demir/Desktop/astro_1024_n_128_l_gllma_d_1.0_s_4,4,4.h5.benchmark.csv"
+  #])
+  #parse_scaling_benchmarks([
+  #  "C:/Users/demir/Desktop/fusion_1024_n_16_l_gllma_d_1.0_s_4,4,4.h5.benchmark.csv",
+  #  "C:/Users/demir/Desktop/fusion_1024_n_32_l_gllma_d_1.0_s_4,4,4.h5.benchmark.csv",
+  #  "C:/Users/demir/Desktop/fusion_1024_n_64_l_gllma_d_1.0_s_4,4,4.h5.benchmark.csv",
+  #  "C:/Users/demir/Desktop/fusion_1024_n_128_l_gllma_d_1.0_s_4,4,4.h5.benchmark.csv"
+  #])
+  #parse_load_balancing_benchmark([
+  #  "C:/Users/demir/Desktop/astro_1024_n_128_l_gllma_d_1.0_s_4,4,4.h5.benchmark.csv"
+  #])
